63105209e265d6b50e54183412fbd6605a5c74a1
Timotej979 <timotej.pet@gmail.com>
2022-05-20 21:51:43 +0200

    Update exercise3.py

--- Exercise 3/exercise3.py
+++ Exercise 3/exercise3.py
@@ -6,7 +6,7 @@ from lpc import lpc_ref, lpc_to_formants, lpc_okno
 from sinsum import sinsum
 
 
-# 2. NALOGA
+# 2. EXERCISE
 Flocal1 = np.array([330, 640, 270, 540, 290, 470, 380, 430])
 Flocal2 = np.array([700,1300,1160,1850,2240,860,2200,1530])
 
@@ -36,10 +36,7 @@ wavfile.write("aeiou-local.wav", fs, siglocal)
 wavfile.write("aeiou-wiki.wav", fs, sigwiki)
 
 
-
-
-
-# 3. NALOGA
+# 3. EXERCISE
 fs, signal = wavfile.read("govor.wav")
 
 # Window size 200 with half overstep (100)
@@ -57,22 +54,8 @@ amplitude = [0] * n_total
 
 finalsig = np.array([0.0])
 
-"""for k in range(n_total):
-    temp_window = signal[(k*n_step):(k*n_step + n_window)]
-    temp_formant, temp_amplitude = lpc_okno(temp_window, order, fs)
-    
-    formant[k] = temp_formant
-    amplitude[k] = temp_amplitude
-
-    temp_sig = sinsum(np.ndarray.tolist(temp_formant), np.ndarray.tolist(temp_amplitude), T_step, fs)
-    
-    finalsig = np.append(finalsig, temp_sig, 0)
-    
-wavfile.write("final.wav", fs, finalsig)
-"""
  
- 
-   
+# 4. EXERCISE 
 finalsig = np.zeros(n_step*(n_total + 1), dtype = np.float32)
     
 for k in range(n_total):
@@ -89,7 +72,3 @@ for k in range(n_total):
     finalsig[k*n_step:k*n_step+n_window] = finalsig[(k*n_step):(k*n_step+n_window)] + temp_sig
     
 wavfile.write("final-hann.wav", fs, finalsig)
-    
-
-
-
